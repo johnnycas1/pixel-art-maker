63838fbeea21012fc71b29d53f1730ef863674d5
Johnny Castillo <johnnycast@gmail.com>
2018-04-30 15:22:05 -0600

    changes made

--- main.js
+++ main.js
@@ -1,52 +1,60 @@
-//THIS IS MY MOTIVATION EVENT!!!! :)////
+//THIS IS MY MOTIVATION EVENT!!!! )
 
 document.addEventListener("DOMContentLoaded",() => {
 alert('YOU CAN DO IT!!!!! Dont give up!!!!');
 });
-////////////////////////////////////////
-//this is my function for the color pallete color picker//
 
-let penColor = "red";
+document.addEventListener("DOMContentLoaded", function() {
+  let canvas = document.getElementsByClassName("canvas")[0]
+  for(let i = 0; i < 10; i++) {
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    canvas.appendChild(pixel);
+  }
+});
 
-    function colorOfPixel(square){
-      square.style.backgroundColor = penColor;
-}
-    function makePenColor(pen) {
-      penColor = pen;
-}
-/////////////////////////////////////////
+let pixels = document.createElement('div');
+pixels.classList.add('pixels');
+canvas.appendChild(pixels);
 
-//this is a mouseover for my titles//
+// let canvas = document.querySelector("canvas");
+//
+// div.addEventListener("click",function(event) {
+//   event.target.className = "canvas";
+// console.log(event.target);
+// })
 
-var title2 = document.getElementById('jc');
-
-title2.addEventListener("mouseover",function( event) {
-  event.target.style.color = "red";
-
-  setTimeout(function(){
-    event.target.style.color = '';
-  }, 500);
-}, false);
-
-var title1 = document.getElementById('colorpallette');
-
-title1.addEventListener("mouseover",function( event) {
-  event.target.style.color = "red";
-
-  setTimeout(function(){
-    event.target.style.color = '';
-  }, 500);
-}, false);
-////////////////////////////////////////
-// this is a test for trying to change the color to white, like an erase button
-
-
-let squares = document.getElementById('pix');
-
-squares.addEventListener("click",function( event) {
-  event.target.style.color = "black";
-
-  setTimeout(function(){
-    event.target.style.color = '';
-  }, 500);
-}, false);
+//
+// let penColor = "red";
+//
+//     function colorOfPixel(canvas){
+//       canvas.style.backgroundColor = penColor;
+// }
+//     function makePenColor(pen) {
+//       penColor = pen;
+// }
+// /////////////////////////////////////////
+//
+// //this is a mouseover for my titles//
+//
+// var title2 = document.getElementById('title');
+//
+// title2.addEventListener("mouseover",function( event) {
+//   event.target.style.color = "red";
+//
+//   setTimeout(function(){
+//     event.target.style.color = '';
+//   }, 500);
+// }, false);
+//
+// var title1 = document.getElementById('colorpallette');
+//
+// title1.addEventListener("mouseover",function( event) {
+//   event.target.style.color = "red";
+//
+//   setTimeout(function(){
+//     event.target.style.color = '';
+//   }, 500);
+// }, false);
+// ////////////////////////////////////////
+// // this is a test for trying to change the color to white, like an erase button
